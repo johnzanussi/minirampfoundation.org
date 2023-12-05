9daf4419a2b505c8d8de3e9061f877b4e8c8d11f
johnzanussi <johnzanussi@gmail.com>
2023-12-04 20:28:28 -0500

    navbar collapse

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="astro/client" />
+
+declare namespace astroHTML.JSX {
+    interface HTMLAttributes {
+        paypalExpress?: string;
+        allowpaymentrequest?: string;
+    }
+}
