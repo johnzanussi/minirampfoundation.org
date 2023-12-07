4b042d78202d4cf183a807e6f862cbe9ec1eb21c
johnzanussi <johnzanussi@gmail.com>
2023-12-06 21:19:35 -0500

    [x] Fix navlink/scroll [x] Cleanup CSS     [x] Set #161616 as $black [x] Lighthouse check [x] Build Compress [x] Mailto link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import purgecss from 'astro-purgecss';
+import compress from 'astro-compress';
+
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
+    integrations: [
+        purgecss(),
+        compress(),
+    ],
 });
---
+++ src/config/index.ts
@@ -0,0 +1,22 @@
+export default {
+    title: 'The Mini Ramp Foundation',
+    url: 'https://minirampfoundation.org',
+    tagline: 'Empowering youth through skateboarding',
+    description: 'The Mini Ramp Foundation is dedicated to providing more public places to skate across the United States. Empowering youth through skateboarding.',
+    navLinks: [
+        {
+            url: '#mission',
+            label: 'Mission'
+        },
+        {
+            url: '#donate',
+            label: 'Donate'
+        },
+        {
+            url: '#contact',
+            label: 'Contact'
+        },
+    ],
+    themeColor: '#161616',
+    contactEmail: 'minirampfoundation@gmail.com',
+};
